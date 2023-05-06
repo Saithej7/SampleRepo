54e7e45cf61a907e805a1d852958a6eb29546e38
saithej7 <saithej747@gmail.com>
2023-05-06 17:13:48 +0530

    first commit

---
+++ Test1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Test1 {
+
+	public static void main(String[] args) {
+		int x=10, y=50;
+		 int big = (x>y) ? x : y;
+		 
+			System.out.println(big);
+			
+			Scanner j = new Scanner(System.in);
+			
+			System.out.println("enter the number :" );
+
+			int num = j.nextInt();
+			int first = num/10;			
+			System.out.println("addition " +(num + x * y));
+
+			System.out.println("Hello World "+ first/10);
+			
+			System.out.println("Welcome to  java");
+	}
+
+}
